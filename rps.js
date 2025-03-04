6320fa963364a6c37787ce89337b1ab0bd7abccf
Richardson <25RichardsonR@badinhs.org>
2025-03-04 13:24:39 -0500

    No progress, dont know how to change random number into a string

---
+++ rps.js
@@ -0,0 +1,60 @@
+/*
+Add steps when you can access google classroom
+
+/*
+let num = Math.random()
+switch(Math.random()){
+        case(num <= .3.toExponential):
+            console.log("Rock")   
+            break;
+    
+        case(.3.toExponential<Math.random()<.6.toExponential):
+           return Paper
+        console.log("Paper")           
+            break;
+
+        case(.6.toExponential<Math.random()):
+        return Scissors
+            console.log("Scissors")
+            break;
+        
+        default:
+                console.log("")
+}
+console.log(num)
+
+/*
+//console.log(console.log(1) && console.log(2));
+
+
+let P = Math.random();
+
+if (P <= .3.toExponential){
+    let P = "Rock"}
+if (.3.toExponential<P<.6.toExponential){
+    let P = "Scissors"}
+if (.6.toExponential<P){
+    let P = "Paper"
+}
+console.log(P)
+
+let num = Math.random()
+console.log(num)
+
+if (num<= .3.toExponential){
+    console.log("Rock")
+if (num)
+}
+*/
+let num = Math.random()
+
+if (num <= .3.toExponential){
+    num = "Rock"
+}
+else if(.3.toExponential<num<.6.toExponential){
+    num = "Scissors"
+}
+else (.6.toExponential<num);{
+    num = "Paper"}
+
+console.log(num)
